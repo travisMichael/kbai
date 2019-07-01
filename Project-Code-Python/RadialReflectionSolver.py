e3a7630e18f6f6b5543ac25459a0d6d6516d8648
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2019-06-30 22:00:32 -0500

    upgraded RadialReflectionSolver

--- Project-Code-Python/RadialReflectionSolver.py
+++ Project-Code-Python/RadialReflectionSolver.py
@@ -28,11 +28,18 @@ def solve_3x3(imageMap):
     if similarity < 0.95:
         return -1
 
-    result = radial_reflection_transform(image_A)
+    best_answer = -1
+    best_similarity = 0.5
 
-    similarity, best_answer = apply_and_check_3x3(result, imageMap)
+    horizontal_reflection_G = horizontal_reflection(image_G)
+
+    for i in range(1, 9):
+        similarity = calculate_image_similarity(horizontal_reflection_G, imageMap.get(str(i)))
+        if similarity > best_similarity:
+            best_similarity = similarity
+            best_answer = i
+
     return best_answer
-    # if similarity > 0.88:
 
 
 def horizontal_reflection(image):
@@ -43,25 +50,27 @@ def vertical_reflection(image):
     return image.transpose(Image.FLIP_TOP_BOTTOM)
 
 
+def calculate(imageMap, reflected_image, label):
+    return calculate_image_similarity(reflected_image, imageMap.get(label))
 # the black pixels of image_2 must be contained inside image 1
-def radial_reflection_transform(image):
-    height, width = image.size
-    image_copy = image.copy()
-
-    np_image = np.array(image)
-    np_copy = np.array(image_copy)
-
-    for i in range(height):
-        for j in range(width):
-            row_column_sum = i + j
-            if row_column_sum < height:
-                diff = height - row_column_sum - 1
-                np_copy[i + diff][j + diff] = np_image[i][j]
-            if row_column_sum > height:
-                diff = row_column_sum % (height - 1)
-                np_copy[i - diff][j - diff] = np_image[i][j]
-
-    return Image.fromarray(np_copy, mode="L")
+# def radial_reflection_transform(image):
+#     height, width = image.size
+#     image_copy = image.copy()
+#
+#     np_image = np.array(image)
+#     np_copy = np.array(image_copy)
+#
+#     for i in range(height):
+#         for j in range(width):
+#             row_column_sum = i + j
+#             if row_column_sum < height:
+#                 diff = height - row_column_sum - 1
+#                 np_copy[i + diff][j + diff] = np_image[i][j]
+#             if row_column_sum > height:
+#                 diff = row_column_sum % (height - 1)
+#                 np_copy[i - diff][j - diff] = np_image[i][j]
+#
+#     return Image.fromarray(np_copy, mode="L")
 
 
 
